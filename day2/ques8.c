a2dd6e331ebf00c22438e1e3a0359d5a88cdaa91
Sahil Singh <sahilthakur2622945@gmail.com>
2026-05-30 01:11:00 +0530

    Day-2 uploaded

---
+++ day2/ques8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,original,reverse=0,digit;
+    printf("enter a number:");
+    scanf("%d",&n);
+    original=n;
+    while(n!=0)
+    {
+        digit=n%10;
+        reverse=reverse*10+digit;
+        n=n/10;
+    }
+    if(original == reverse)
+    {
+        printf("number is palindrome");
+    }
+    else
+    {
+        printf("number is not a palindrome");
+    }
+    return 0;
+}
