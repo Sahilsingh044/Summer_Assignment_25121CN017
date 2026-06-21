c479cd09bb78f858d4e133ffaaa70f4853618469
Sahil Singh <sahilthakur2622945@gmail.com>
2026-06-21 23:25:04 +0530

    day-15 uploaded

---
+++ day15/ques59.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,temp;
+    scanf("%d",&n);
+
+    int arr[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    temp=arr[n-1];
+
+    for(i=n-1;i>0;i--)
+        arr[i]=arr[i-1];
+
+    arr[0]=temp;
+
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+}
