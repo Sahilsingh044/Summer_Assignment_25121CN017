f71c999badff0dff31c033fb229e522c55648fac
Sahil Singh <sahilthakur2622945@gmail.com>
2026-06-29 00:33:48 +0530

    day 28 uploaded

---
+++ day28/ques111.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int seats = 50, book;
+
+    printf("Available Seats = %d\n", seats);
+
+    printf("Enter Seats to Book: ");
+    scanf("%d", &book);
+
+    if(book <= seats) {
+        seats -= book;
+        printf("Booking Successful\n");
+        printf("Remaining Seats = %d", seats);
+    }
+    else {
+        printf("Seats Not Available");
+    }
+
+    return 0;
+}
